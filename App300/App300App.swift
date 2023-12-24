b7b6185e8fc62ada687c0f8d4a852e86b6b5a980
spustoff <vyacheslavpustov05@gmail.com>
2023-12-24 11:39:07 +0300

    Initial Commit

---
+++ App300/App300App.swift
@@ -0,0 +1,17 @@
+//
+//  App300App.swift
+//  App300
+//
+//  Created by Вячеслав on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct App300App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
